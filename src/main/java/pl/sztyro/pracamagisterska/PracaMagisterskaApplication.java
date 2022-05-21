eee15add2c7f74eaa149cc2acbdf44bafac05d25
SZtyro <fabixd123@gmail.com>
2022-05-21 10:05:58 +0200

    Init

---
+++ src/main/java/pl/sztyro/pracamagisterska/PracaMagisterskaApplication.java
@@ -0,0 +1,13 @@
+package pl.sztyro.pracamagisterska;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracaMagisterskaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracaMagisterskaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/sztyro/pracamagisterska/PracaMagisterskaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sztyro.pracamagisterska;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracaMagisterskaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
